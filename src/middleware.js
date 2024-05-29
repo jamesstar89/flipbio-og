85c9db21a8e3b924e2abf7200741dd5c9aab8ad8
James Star <jamess.developer@gmail.com>
2024-05-30 07:57:06 +1200

    Update

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,4 @@
 import { NextResponse } from 'next/server';
-import { MiddlewareResponse } from '@netlify/next';
 
 // https://www.propelauth.com/post/getting-url-in-next-server-components
 // https://docs.netlify.com/frameworks/next-js/runtime-v4/middleware/
@@ -8,9 +7,7 @@ import { MiddlewareResponse } from '@netlify/next';
 export function middleware(request) {
   const headers = new Headers(request.headers);
   headers.set('x-current-path', request.nextUrl.pathname);
-  const req = MiddlewareResponse.next({ headers });
-  // return NextResponse.next({ headers });
-  return req;
+  return NextResponse.next({ headers });
 }
 
 export const config = {

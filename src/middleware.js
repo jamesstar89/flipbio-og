fdfe59e6d4b4ffbffdb8da6b3274c29c0dd9516f
James Star <jamess.developer@gmail.com>
2024-05-29 21:12:05 +1200

    Add netlify toml file

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,13 @@
 import { NextResponse } from 'next/server';
-import { MiddlewareResponse } from '@netlify/next';
 
 // https://www.propelauth.com/post/getting-url-in-next-server-components
 // https://docs.netlify.com/frameworks/next-js/runtime-v4/middleware/
 // https://www.netlify.com/with/nextjs/
 
-export function middleware(nextRequest) {
-  const headers = new Headers(nextRequest.headers);
-  headers.set('x-current-path', nextRequest.nextUrl.pathname);
-  const request = new MiddlewareResponse(NextResponse.next({ headers }));
-  return request;
+export function middleware(request) {
+  const headers = new Headers(request.headers);
+  headers.set('x-current-path', request.nextUrl.pathname);
+  return NextResponse.next({ headers });
 }
 
 export const config = {
